0d6beac7ccd8dfe04fe5075bccea6608aeaec6af
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-10-09 21:08:51 +0900

    [ 🎉 | #1 ] 초기세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      white: '#ffffff',
+      black_22: '#222222',
+      lightGray: '#f5f5f5',
+    },
   },
   plugins: [],
-}
+};
