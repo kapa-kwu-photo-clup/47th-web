3e5547b09432afe1919e5d0f031061170d9acbad
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-10-10 14:18:01 +0900

    [ 🎉 | #1 ] 초기세팅

--- src/layout/Header/header.tsx
+++ src/layout/Header/header.tsx
@@ -1,14 +1,48 @@
+import { useLocation, Link } from 'react-router-dom';
+
 export default function Header() {
+  const location = useLocation();
+
+  const getHeaderColor = (path: string) => {
+    switch (path) {
+      case '/':
+        return 'bg-springPink';
+      case '/come':
+        return 'bg-summerGreen_01';
+      case '/exhibition':
+        return 'bg-fallPurple';
+      case '/event':
+        return 'bg-winterBlue';
+      default:
+        return 'bg-lightGray';
+    }
+  };
+
   return (
-    <>
-      <header className="w-full bg-lightGray">
-        <div className=" gap-4 flex flex-row">
-          <a href="./">메인</a>
-          <a href="./come">VISIT</a>
-          <a href="./exhibition">EXHIBITION</a>
-          <a href="./event">EVENT</a>
-        </div>
-      </header>
-    </>
+    <header className={`w-full ${getHeaderColor(location.pathname)}`}>
+      <div className="gap-4 flex flex-row">
+        <Link to="/" className={location.pathname === '/' ? 'font-bold' : ''}>
+          메인
+        </Link>
+        <Link
+          to="/come"
+          className={location.pathname === '/come' ? 'font-bold' : ''}
+        >
+          VISIT
+        </Link>
+        <Link
+          to="/exhibition"
+          className={location.pathname === '/exhibition' ? 'font-bold' : ''}
+        >
+          EXHIBITION
+        </Link>
+        <Link
+          to="/event"
+          className={location.pathname === '/event' ? 'font-bold' : ''}
+        >
+          EVENT
+        </Link>
+      </div>
+    </header>
   );
 }
