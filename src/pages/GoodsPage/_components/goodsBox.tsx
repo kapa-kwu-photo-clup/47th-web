af3fcd650d3632be50c8402ef2880caf30b4cf9d
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-10-26 16:26:20 +0900

    [ 🎨 | style ] 디자인 요소 추가 css 변경

--- src/pages/GoodsPage/_components/goodsBox.tsx
+++ src/pages/GoodsPage/_components/goodsBox.tsx
@@ -12,11 +12,8 @@ export default function GoodsBox({
   goodsPrice,
 }: GoodsBoxProps) {
   return (
-    <div className="flex flex-col gap-5">
-      <img
-        className="lg:w-[20.5rem] lg:h-[20.5rem] w-40 h-40 mb-4"
-        src={imgSrc}
-      />
+    <div className="flex flex-col p-5">
+      <img className="object-cover aspect-square mb-4" src={imgSrc} />
       <p className="text-xl font-bold mb-1">{goodsName}</p>
       <p className="text-xl font-base mb-8">{goodsSubtitle}</p>
       <p className="text-xl font-bold">{goodsPrice}</p>
--- src/pages/GoodsPage/goodsPage.tsx
+++ src/pages/GoodsPage/goodsPage.tsx
@@ -3,6 +3,8 @@ import { useMediaQuery } from 'react-responsive';
 import GoodsImg from '../../assets/images/jpg/goodsImg.jpg';
 import GoodsImgMobile from '../../assets/images/jpg/GoodsImgMobile.jpg';
 import GoodsBox from './_components/goodsBox';
+import keyRingImgSrc from '../../assets/images/webp/키링 이미지.webp';
+import PostcardImgSrc from '../../assets/images/webp/엽서 이미지.webp';
 export default function GoodsPage() {
   useScrollToTop();
 
@@ -15,26 +17,31 @@ export default function GoodsPage() {
         src={isLargeScreen ? GoodsImg : GoodsImgMobile}
       />
       <div className=" lg:w-3/4 lg:py-0 lg:px-0 px-5 mx-auto">
-        <div className=" flex flex-wrap gap-[2.625rem] justify-center mb-40">
-          <GoodsBox
-            imgSrc={GoodsImg}
-            goodsName="2025 미니 달력"
-            goodsSubtitle="사계절을 담은 달력"
-            goodsPrice="5,000원"
-          />
-          <GoodsBox
-            imgSrc={GoodsImg}
-            goodsName="필름 키링"
-            goodsSubtitle="달랑달랑 필름 키링"
-            goodsPrice="500원"
-          />
-          <GoodsBox
-            imgSrc={GoodsImg}
-            goodsName="엽서"
-            goodsSubtitle="전시작을 담은 엽서"
-            goodsPrice="무료"
-          />
-          <GoodsBox imgSrc="" goodsName="" goodsSubtitle="" goodsPrice="" />
+        <div className=" flex flex-wrap mb-40">
+          <div className="lg:w-1/4 w-1/2 lg:mb-0 mb-11">
+            <GoodsBox
+              imgSrc={GoodsImg}
+              goodsName="2025 미니 달력"
+              goodsSubtitle="사계절을 담은 달력"
+              goodsPrice="5,000원"
+            />
+          </div>
+          <div className="lg:w-1/4 w-1/2 lg:mb-0 mb-11">
+            <GoodsBox
+              imgSrc={keyRingImgSrc}
+              goodsName="필름 키링"
+              goodsSubtitle="달랑달랑 필름 키링"
+              goodsPrice="500원"
+            />
+          </div>
+          <div className="lg:w-1/4 w-1/2 lg:mb-0 mb-11">
+            <GoodsBox
+              imgSrc={PostcardImgSrc}
+              goodsName="엽서"
+              goodsSubtitle="전시작을 담은 엽서"
+              goodsPrice="무료"
+            />
+          </div>{' '}
         </div>
       </div>
     </>
