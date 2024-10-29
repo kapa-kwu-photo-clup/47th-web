c220f6dcee49d2623c1f9e8f6119ae3cba4d2a01
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-10-29 18:13:34 +0900

    [ 🚨 | Fix ] fix build err

--- src/pages/exhibitionPage/_components/imgWrapper.tsx
+++ src/pages/exhibitionPage/_components/imgWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { pictureData } from '../../../assets/data/pictureData';
 import ImageCard from './imgCard';
 import Modal from './modal'; // Modal 컴포넌트 임포트
@@ -18,8 +18,23 @@ interface ImgWrapperProps {
   season: string;
 }
 
+// pictureData의 타입을 정의합니다.
+interface PictureData {
+  season: string;
+  flag: number;
+  name: string;
+  dept: string;
+  camera: string;
+  title: string;
+  content: string;
+  address: string;
+}
+
+// pictureData의 타입을 명시합니다.
 export default function ImgWrapper({ season }: ImgWrapperProps) {
-  const seasonImg = pictureData.filter((item) => item.season === season);
+  const seasonImg = pictureData.filter(
+    (item: PictureData) => item.season === season,
+  ); // item의 타입을 명시
 
   // 모달 상태 관리
   const [selectedImage, setSelectedImage] = useState<Picture | null>(null);
@@ -38,9 +53,18 @@ export default function ImgWrapper({ season }: ImgWrapperProps) {
   return (
     <>
       <div className="flex flex-wrap my-24">
-        {seasonImg.map((item, index) => (
-          <ImageCard key={index} item={item} onClick={() => openModal(item)} />
-        ))}
+        {seasonImg.map(
+          (
+            item: PictureData,
+            index: number, // item과 index의 타입을 명시
+          ) => (
+            <ImageCard
+              key={index}
+              item={item}
+              onClick={() => openModal(item)}
+            />
+          ),
+        )}
       </div>
       <Modal isOpen={isModalOpen} onClose={closeModal} image={selectedImage} />
     </>
