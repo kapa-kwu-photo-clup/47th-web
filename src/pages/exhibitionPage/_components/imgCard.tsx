688580f3dcc765bf7fff52079129627357c4e445
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-11-03 00:45:55 +0900

    [ ✨ | #39 ] 사진 스켈레톤ui 추가

--- src/pages/exhibitionPage/_components/imgCard.tsx
+++ src/pages/exhibitionPage/_components/imgCard.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Picture } from './imgWrapper';
 
 interface ImageCardProps {
   item: Picture;
-  onClick: () => void; // 클릭 핸들러를 props로 받음
+  onClick: () => void;
 }
 
 const ImageCard: React.FC<ImageCardProps> = ({ item, onClick }) => {
+  const [loading, setLoading] = useState(true);
+
+  // 이미지 주소가 변경될 때 로딩 상태를 true로 설정하여 로딩 시작을 표시
+  useEffect(() => {
+    setLoading(true);
+  }, [item.address]);
+
+  // 이미지 로딩 성공 시 로딩 상태를 false로 변경
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
+
+  // 이미지 로딩 실패 시 로딩 상태를 true로 변경
+  const handleImageError = () => {
+    setLoading(true);
+  };
+
   return (
-    <div className={`w-1/2 lg:w-1/4 relative lg:p-[1.35rem]`}>
+    <div
+      className={`w-1/2 lg:w-1/4 relative lg:p-[1.35rem] ${
+        loading ? 'animate-pulse' : ''
+      }`}
+    >
       <img
         className="w-full h-full object-cover p-3 lg:hover:p-0 transition-all duration-300 aspect-square lg:cursor-pointer"
-        src={item.address} // 이미지 주소를 props로 받음
+        src={item.address}
         alt={`${item.season}-${item.flag}기-${item.name}-${item.title}`}
-        onClick={onClick} // 클릭 이벤트 추가
+        onClick={onClick}
+        onLoad={handleImageLoad}
+        onError={handleImageError}
       />
     </div>
   );
